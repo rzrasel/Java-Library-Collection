f54198a0d1fab5a76ddf036f9a460c425a3bf05c
Rz Rasel <rzrasel@users.noreply.github.com>
2017-11-01 17:18:09 +0600

    Update SQLiteConnection.java

--- DBConnection/SQLiteConnection.java
+++ DBConnection/SQLiteConnection.java
@@ -1,5 +1,14 @@
-package com.rz.Libraries.DBConnection;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rz.libraries;
 
+/**
+ *
+ * @author Rz Rasel
+ */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,7 +19,57 @@ import java.util.logging.Logger;
 
 /**
  *
+ * <p>
+ * A public class for <b>SQLite connection</b>. <i>Support singleton
+ * pattern</i>, also create an object using <i>new keyword</i>.
+ * 
+ * <br /><br />
+ * - Set up SQLite JDBC driver. Download latest version of
+ * sqlite-jdbc-(VERSION).jar from sqlite-jdbc repository.
+ * <ul>
+ * <li>getInstance - SQLiteConnection.getInstance(String argDbFilePath)</li>
+ * <li>Create an object using new Keyword new SQLiteConnection(String
+ * argDbFilePath)</li>
+ * </ul>
+ * 
+ * <br />
+ * - <b>Methods</b> inside this class:
+ * <ul>
+ * <li>public static SQLiteConnection
+ * {@link #getInstance(String argDbFilePath) getInstance(String argDbFilePath)}</li>
+ * <li>public
+ * {@link #SQLiteConnection(String argDbFilePath) SQLiteConnection(String argDbFilePath)}</li>
+ * <li>public Connection {@link #onOpenConnection() onOpenConnection()}</li>
+ * <li>public void
+ * {@link #onExecuteRawQuery(String argSqlQuery) onExecuteRawQuery(String argSqlQuery)}</li>
+ * <li>public ResultSet
+ * {@link #onSqlQuery(String argSqlQuery) onSqlQuery(String argSqlQuery)}</li>
+ * <li>public void
+ * {@link #onCloseResultSet(ResultSet argResultSet) onCloseResultSet(ResultSet argResultSet)}</li>
+ * <li>public void {@link #onCloseStatement() onCloseStatement()}</li>
+ * <li>public void
+ * {@link #onCloseStatement(Statement argStatement) onCloseStatement(Statement argStatement)}</li>
+ * <li>public void {@link #onCloseConnection() onCloseConnection()}</li>
+ * <li>public void {@link #onClose() onClose()}</li>
+ * </ul>
+ * 
+ * See {@link
+ * <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1">HTTP/1.1
+ * documentation</a>}. null {@link package.class#member label}
+ * {@link com.rz.libraries.SQLiteConnection#getInstance(java.lang.String)}
+ * {@link getFoo()}
+ * {@link #getInstance(String argDbFilePath) getInstance(String argDbFilePath)}
+ * 
+ * <br /><br />
+ * Note: Additional information, e.g. your implementation notes or remarks.
+ * </p>
+ *
+ * @param argDbFilePath use String SQLite database path
+ * @version 20171101.0.1
  * @author Rz Rasel
+ * @since V-20171101.0.1
+ * @see <a href="https://bitbucket.org/xerial/sqlite-jdbc/downloads/">SQLite-Jdbc</a>
+ * @note If you spin a quark, it will spin forever!
  */
 public class SQLiteConnection {
 
@@ -21,6 +80,15 @@ public class SQLiteConnection {
     private Statement statement = null;
     private ResultSet resultSet = null;
 
+    /**
+     * <p>
+     * A public static method
+     * {@link #getInstance(String argDbFilePath) getInstance(String argDbFilePath)}
+     * </p>
+     *
+     * @param argDbFilePath use String SQLite database path
+     * @return SQLiteConnection
+     */
     public static SQLiteConnection getInstance(String argDbFilePath) {
         if (instance == null) {
             instance = new SQLiteConnection(argDbFilePath);
@@ -28,10 +96,25 @@ public class SQLiteConnection {
         return instance;
     }
 
+    /**
+     * <p>
+     * A public method
+     * {@link #SQLiteConnection(String argDbFilePath) SQLiteConnection(String argDbFilePath)}
+     * </p>
+     *
+     * @param argDbFilePath use String SQLite database path
+     */
     public SQLiteConnection(String argDbFilePath) {
         DB_URL += argDbFilePath;
     }
 
+    /**
+     * <p>
+     * A public method
+     * {@link #onOpenConnection() onOpenConnection()}
+     * </p>
+     * @return Connection
+     */
     public Connection onOpenConnection() {
         try {
             //String url = "jdbc:sqlite:test.sqlite3";
@@ -124,4 +207,4 @@ if (c.moveToFirst())
            c.moveToNext();
         }
     }
-*/
+ */
