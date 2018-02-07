e6639dc856b73e30aec831835b2a520335e4b85a
Rz Rasel <Rz Rasel>
2018-02-07 10:14:59 +0600

    Java SQL Generator V-201801.00.3 - 2018-02-07 10:14:50.80

--- src/AppPlayStoreDataFormat.java
+++ src/AppPlayStoreDataFormat.java
@@ -2,12 +2,16 @@
 import com.rz.librarycore.RandomValue;
 import com.rz.librarycore.dbhandler.SQLiteConnection;
 import com.rz.librarycore.logger.LogWriter;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,17 +23,52 @@ import java.util.Date;
  * @author Rz Rasel
  */
 public class AppPlayStoreDataFormat {
-    
+
     public static final String DB_NAME = "app-jar-libs/app-system.sqlite3";
     private SQLiteConnection sQLiteConnection;
     private String sqlQuery = "";
-    
+
+    public String convertStringToHex(String str) {
+
+        char[] chars = str.toCharArray();
+
+        StringBuffer hex = new StringBuffer();
+        for (int i = 0; i < chars.length; i++) {
+            hex.append(Integer.toHexString((int) chars[i]));
+        }
+
+        return hex.toString();
+    }
+
+    public String convertHexToString(String hex) {
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+
+        //49204c6f7665204a617661 split into two characters 49, 20, 4c...
+        for (int i = 0; i < hex.length() - 1; i += 2) {
+
+            //grab the hex in pairs
+            String output = hex.substring(i, (i + 2));
+            //convert hex to decimal
+            int decimal = Integer.parseInt(output, 16);
+            //convert the decimal to character
+            sb.append((char) decimal);
+
+            temp.append(decimal);
+        }
+        System.out.println("Decimal : " + temp.toString());
+
+        return sb.toString();
+    }
+
     public static void main(String args[]) {
         AppPlayStoreDataFormat appPlayStoreDataFormat = new AppPlayStoreDataFormat();
         appPlayStoreDataFormat.onDataFormat();
     }
-    
+
     private void onDataFormat() {
+        boolean isPrintMySql = false;
         Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String currentDate = formatter.format(new Date());
         System.out.println("DEBUG: " + currentDate);
@@ -39,12 +78,8 @@ public class AppPlayStoreDataFormat {
             ResultSet resultSet = sQLiteConnection.onSqlQuery(sqlQuery);
             try {
                 if (resultSet != null) {
-                    System.out.println("");
+                    System.out.println("\n\n");
                     while (resultSet.next()) {
-                        String insertSqlData = "INSERT INTO tbtmp_app_play_store "
-                                + " VALUES ("
-                                + "%s, %s, %s, %s, '%s', '%s'"
-                                + ");";
                         currentDate = formatter.format(new Date());
                         String colTitle = resultSet.getString("apstre_aplstor_title").trim();
                         String colDescription = resultSet.getString("apstre_aplstor_description").trim();
@@ -53,38 +88,81 @@ public class AppPlayStoreDataFormat {
                         String colApkURL = resultSet.getString("apstre_aplstor_apk_url").trim();
                         String colSEO = resultSet.getString("apstre_aplstor_seo").trim();
                         String colTAG = resultSet.getString("apstre_aplstor_tag").trim();
-                        String colIsFeatured = resultSet.getString("apstre_aplstor_is_featured").trim();
-                        String colIsOwn = resultSet.getString("apstre_aplstor_is_own").trim();
-                        String colStatus = resultSet.getString("apstre_aplstor_status").trim();
-                        String colCreateDate = resultSet.getString("apstre_aplstor_create_date").trim();
+                        String colIsFeatured = resultSet.getInt("apstre_aplstor_is_featured") + "";
+                        String colIsOwn = resultSet.getInt("apstre_aplstor_is_own") + "";
+                        String colStatus = resultSet.getInt("apstre_aplstor_status") + "";
+                        //String colCreateDate = resultSet.getString("apstre_aplstor_create_date").trim();
                         String newId = "";
                         newId = RandomValue.getRandId(1111, 9999);
                         newId = Utils.getDbFromat(newId);
-                        colTitle = Utils.getDbFromat(Utils.toProperCase(colTitle));
-                        colDescription = Utils.getDbFromat(colDescription);
+                        //String escapedString = newInsertSql.replace("'", "''");
+                        /*colTitle = colTitle.replace("'", "\\'");
+                        colDescription = colDescription.replace("'", "\\'");*/
                         colSlug = Utils.toEmptyToNull(colSlug);
                         if (colSlug == null) {
                             colSlug = Utils.toSlugCase(colTitle + "");
                         } else {
                             colSlug = Utils.toSlugCase(colSlug + "");
                         }
+                        if (isPrintMySql) {
+                            colTitle = Utils.htmlEntityCode(Utils.toProperCase(colTitle));
+                            colDescription = Utils.htmlEntityCode(colDescription);
+                            colTitle = colTitle.replaceAll("&#32;", " ");
+                            colDescription = colDescription.replaceAll("&#32;", " ");
+                        } else {
+                            //colTitle = colTitle.replaceAll("'","''");
+                            String regexStr = "(?=[]\\[+&|!(){}^\"~*?:\\\\-])";
+                            //colTitle = colTitle.replaceAll(regexStr, "\\\\");
+                            //colDescription = colDescription.replaceAll(regexStr, "\\\\");
+                            /*colTitle = Utils.urlEncode(colTitle);
+                            colDescription = Utils.urlEncode(colDescription);*/
+                            regexStr = "['\\+]";
+                            colTitle = "+-*/\"\\";
+                            colTitle = "\\";
+                            colTitle = Utils.escapeSqlSpecial(colTitle);
+                            //colDescription = Utils.escapeSqlSpecial(colDescription);
+                        }
+                        colTitle = Utils.getDbFromat(Utils.toProperCase(colTitle));
+                        colDescription = Utils.getDbFromat(colDescription);
                         colSlug = Utils.getDbFromat(colSlug);
                         colImageURL = Utils.getDbFromat(colImageURL);
                         colApkURL = Utils.getDbFromat(colApkURL);
                         colSEO = Utils.getDbFromat(colSEO);
+                        colTAG = Utils.getDbFromat(colTAG);
                         //LogWriter.Log("TEST: " + colTitle.substring(0, 6) + " - " + colSlug + "");
                         //LogWriter.Log("TEST: " + Utils.toSlugCase(colTitle + ""));
-                        String newInsertSql = String.format(insertSqlData,
-                                newId,
-                                colTitle,
-                                colDescription,
-                                colSlug,
-                                currentDate,
-                                currentDate
-                        );
+                        String newInsertSql = "";
+                        if (isPrintMySql) {
+                            String insertSqlData = "INSERT INTO tbtmp_app_play_store "
+                                    + " VALUES ("
+                                    + "%s, %s, %s, %s,"
+                                    + " %s, %s, %s,"
+                                    + " %s, %s, %s, %s,"
+                                    + " '%s', '%s'"
+                                    + ");";
+                            newInsertSql = String.format(insertSqlData,
+                                    newId, colTitle, colDescription, colSlug,
+                                    colImageURL, colApkURL, colSEO,
+                                    colTAG, colIsFeatured, colIsOwn, colStatus,
+                                    currentDate, currentDate
+                            );
+                        } else {
+                            String insertSqlData = "INSERT INTO tbtmp_app_play_store "
+                                    + " VALUES ("
+                                    + "%s, %s, %s, %s,"
+                                    + " %s, %s, %s,"
+                                    + " %s, %s, %s, %s"
+                                    + ");";
+                            newInsertSql = String.format(insertSqlData,
+                                    newId, colTitle, colDescription, colSlug,
+                                    colImageURL, colApkURL, colSEO,
+                                    colTAG, colIsFeatured, colIsOwn, colStatus
+                            );
+                        }
                         System.out.println(newInsertSql);
                         Thread.sleep(200);
                     }
+                    System.out.println("\n\n");
                 }
             } catch (SQLException e) {
                 System.out.println("SQLException: " + e.toString());
@@ -97,12 +175,12 @@ public class AppPlayStoreDataFormat {
             onDataFormat();
         }
     }
-    
+
     private void openDatabase() {
         sQLiteConnection = SQLiteConnection.getInstance(DB_NAME);
         Connection conn = sQLiteConnection.onOpenConnection();
     }
-    
+
     private void closeDatabase() {
         sQLiteConnection.onCloseStatement();
         sQLiteConnection.onClose();
