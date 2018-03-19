230573f31147c5ae08987d7405ddcc1df3709bcb
Developer <developer@Shurjo.local>
2018-03-19 17:08:39 +0600

    Java SQL Generator V-201802.02.3 - 2018-03-19 17:08:38

--- And-APP-Test-Example/appusagesexample/src/main/java/com/rz/usagesexample/ActSplash.java
+++ And-APP-Test-Example/appusagesexample/src/main/java/com/rz/usagesexample/ActSplash.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rz.librarycore.apppackage.APPStaticPackageInfo;
+import com.rz.librarycore.log.SecureKeyManager;
+import com.rz.librarycore.storage.SharePrefPrivateHandler;
+
 public class ActSplash extends AppCompatActivity {
     private Activity activity;
     private Context context;
@@ -16,7 +20,12 @@ public class ActSplash extends AppCompatActivity {
         setContentView(R.layout.act_splash);
         activity = this;
         context = this;
+        SharePrefPrivateHandler sharePrefHandler = new SharePrefPrivateHandler(context, APPStaticPackageInfo.getPackageName(context));
+        sharePrefHandler.clearAll();
+        SecureKeyManager secureKeyManager = new SecureKeyManager(activity, context);
+        SecureKeyManager.onSetAppIsRunFirstTime(context);
         startActivity(new Intent(context, ActYoutubeTest.class));
+        startService(new Intent(getBaseContext(), OnServiceTest.class));
         finish();
     }
 }
